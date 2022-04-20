2246af3cfbe5f07ff99c9ad54c44e48d367712e6
kana-rus <kanarus786@gmail.com>
2022-04-20 21:09:58 +0900

    "AC: 746byte 184ms 8140KB"

---
+++ 010.rs
@@ -0,0 +1,45 @@
+use proconio::input;
+
+fn main() {
+    input!{
+        n: usize,
+        class_point: [(usize, usize); n],
+        q: usize,
+        l_r: [(usize, usize); q]
+    }
+
+    /*
+    1組、2組それぞれについて、「学籍番号 i までの得点の累積和」
+    をメモしておく。これにより、「x 番から y 番までの得点の和」を
+                    sum[y] - sum[x-1]
+    で求められる。
+    */
+    let (sum1, sum2) = {
+        let mut s1 = vec![0; 1+n];
+        let mut s2 = vec![0; 1+n];
+
+        for i in 1..=n {
+            s1[i] = s1[i-1];
+            s2[i] = s2[i-1];
+            let data = class_point[i-1];
+            if data.0 == 1 {
+                s1[i] += data.1;
+            } else {
+                s2[i] += data.1;
+            }
+        }
+
+        (s1, s2)
+    };
+
+    for query in l_r {
+        println!("{} {}",
+            sum1[query.1] - sum1[query.0 - 1],
+            sum2[query.1] - sum2[query.0 - 1]
+            /*
+            sum1, sum2 で 0 番要素は 0 としているので、
+            query.0 が 1(番) でもうまくいく。
+            */
+        );
+    }
+}
